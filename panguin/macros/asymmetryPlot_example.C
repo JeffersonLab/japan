e7cb671f1e78fe2d2e9ca7953156e41cb7425f51
Cameron Clarke <cameronc137@gmail.com>
2019-02-02 17:45:32 -0500

    I have gotten to the level of modularly generating analyzed data and reusing it in subsequent PANGUIN plots. However, the macro's memory persists, subsequent re-use from succeeding.

--- panguin/macros/asymmetryPlot_example.C
+++ panguin/macros/asymmetryPlot_example.C
@@ -20,38 +20,68 @@
 #include <string>
 #include <vector>
 #include "TTree.h"
+//#include "japantypes.hh"
 
 void asymmetryPlot_example(std::string channel) {
 
     gStyle->SetOptStat(0);
 
     // To grab the root tree output we can assume that PANGUIN has successfully opened a root file
-    TTree *oldPatternTree = (TTree*)gDirectory->Get("Hel_Tree");
+    TFile *oldFile = gFile;
+    TTree *oldPatternTree = (TTree*)oldFile->Get("Hel_Tree");
+    std::string dir = (std::string)gDirectory->CurrentDirectory()->GetPath();
     // Find out how many entries are in the tree so we can loop over the pattern tree later
     Int_t numEntries = oldPatternTree->GetEntries();
     // Make a new ROOT tree to store the new data in (this is just an academic example)
-    TTree *newPatternTree = new TTree("noise_Hel_Tree","Tree of Pattern based noise");
-    
-    // Define a new struct that will hold the individual leaves from the branch of interest
-    typedef struct {Double_t hw_sum, block[4],numsamp,errorcode;} PATTERNASYMS;
-    
-    // Make an instance of the struct
-    PATTERNASYMS patternAsyms;
-    // Assign that instance to the new ROOT tree's branch location of interest
-    oldPatternTree->SetBranchAddress( Form("asym_%s",channel.c_str()), &patternAsyms );
+    TTree *newPatternTree;
+    TFile *resultsFile;
+    std::string outputFile = Form("second_pass_%s.root",channel.c_str());
+    // Check to see if analysis has been run before
+    if(gSystem->AccessPathName(outputFile.c_str())){
+        // Write a new root file for output 
+        resultsFile = new TFile(outputFile.c_str(),"RECREATE");
+        // Create a new tree to store results in
+        newPatternTree = new TTree("noise_Hel_Tree","Tree of Pattern based noise");
 
-    // Make a double to hold the data that is the result of our manipulation
-    Double_t patternNoises; // call the asymmetry "noise", as an unblinded asym is also the noise floor
-    // Make a new branch to hold the data, but this object is ideally not manipulated much
-    TBranch *patternNoisesBranch;
-    // Assign that branch to hold the double we made earlier as the contents of each entry
-    patternNoisesBranch = newPatternTree->Branch(Form("pattern_noise_%s",channel.c_str()), &patternNoises);
-    for (int j = 0; j < numEntries; j++) {
-        // Loop over the input file's entries and fill the new tree with the results of one of the leaves
-        oldPatternTree->GetEntry(j);
-        patternNoises=patternAsyms.hw_sum;
-        newPatternTree->Fill();
+        // Make an instance of the struct we need to read the old Tree's data
+        typedef struct {
+            Double_t hw_sum;
+            Double_t block0;
+            Double_t block1;
+            Double_t block2;
+            Double_t block3;
+            Double_t numsamp;
+            Double_t errorcode;
+        } PATTERNASYMS;
+        PATTERNASYMS patternAsyms;
+        // Assign that instance to the new ROOT tree's branch location of interest
+        oldPatternTree->SetBranchAddress( Form("asym_%s",channel.c_str()), &patternAsyms );
+
+        // Make a double to hold the data that is the result of our manipulation
+        Double_t patternNoises; // call the asymmetry "noise", as an unblinded asym is also the noise floor
+        // Make a new branch to hold the data, but this object is ideally not manipulated much
+        TBranch *patternNoisesBranch;
+        // Assign that branch to hold the double we made earlier as the contents of each entry
+        patternNoisesBranch = newPatternTree->Branch(Form("pattern_noise_%s",channel.c_str()), &patternNoises);
+        for (int j = 0; j < numEntries; j++) {
+            // Loop over the input file's entries and fill the new tree with the results of one of the leaves
+            oldPatternTree->GetEntry(j);
+            patternNoises=patternAsyms.hw_sum;
+            newPatternTree->Fill();
+        }
+        // Save your root tree for later use
+        resultsFile->Write();
+        newPatternTree->Draw(Form("pattern_noise_%s",channel.c_str()));
     }
     // Draw your results
-    newPatternTree->Draw(Form("pattern_noise_%s",channel.c_str()));
+    else{
+        resultsFile = new TFile(outputFile.c_str(),"READ");
+        newPatternTree = (TTree*)resultsFile->Get("noise_Hel_Tree");
+        newPatternTree->Draw(Form("pattern_noise_%s",channel.c_str()));
+    }
+
+    delete newPatternTree;
+    resultsFile->Close();
+    delete resultsFile;
+    gFile=oldFile;
 }
