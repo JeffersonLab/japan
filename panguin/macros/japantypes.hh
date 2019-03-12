1e1ad3c79f642655f2d682f7ba1cf4eef5166516
Cameron Clarke <cameronc137@gmail.com>
2019-03-12 16:14:19 -0400

    Undoing the cleanup commit to allow for merging with operations later

---
+++ panguin/macros/japantypes.hh
@@ -0,0 +1,19 @@
+// Define a new struct that will hold the individual leaves from the branch of interest
+typedef struct {
+    Double_t hw_sum;
+    Double_t block0;
+    Double_t block1;
+    Double_t block2;
+    Double_t block3;
+    Double_t numsamp;
+    Double_t errorcode;
+} PATTERNASYMS;
+typedef struct {
+	Double_t hw_sum;
+	Double_t block0;
+	Double_t block1;
+	Double_t block2;
+	Double_t block3;
+	Double_t numsamp;
+	Double_t errorcode;
+} EVTTREE;
