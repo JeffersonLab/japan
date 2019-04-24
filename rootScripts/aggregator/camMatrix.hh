eb695085c2fa469818ad8cdfea7561817314cdd2
Cameron Clarke <cameronc137@gmail.com>
2019-04-24 14:07:06 -0400

    Updating regression code to utilize matrices correctly and use TTreeReader

--- rootScripts/aggregator/camMatrix.hh
+++ rootScripts/aggregator/camMatrix.hh
@@ -5,39 +5,87 @@
 #include <TString.h>
 #include <iostream>
 #include <string>
+// Utilizing code from: https://www.geeksforgeeks.org/adjoint-inverse-matrix/
 using namespace std;
-void displayMatrix_h(vector<vector<Double_t>> input){
-  Printf("(");
+void displayVector_h(vector<Double_t> input){
+  cout<<"{";
   for (Int_t j = 0; j<input.size(); j++){
-    Printf("(");
-    for (Int_t k = 0; k<input[j].size(); k++){
-      Printf("\t%f\t",input[j][k]);
+    cout<<" "<<Form("%4.2e",input[j]);
+    if (j<input.size()-1){
+      cout<<"\n";
     }
-    Printf(")\n");
   }
-  Printf(")");
+  cout<<" }\n";
 }
 
-void cofactor_h(vector<vector<Double_t>> input, vector<vector<Double_t>> temp, Int_t inRow, Int_t inColumn, Int_t length){
+void displayMatrix_h(vector<vector<Double_t>> input){
+  cout<<"{";
+  for (Int_t j = 0; j<input.size(); j++){
+    cout<<"{";
+    for (Int_t k = 0; k<input[j].size(); k++){
+      //cout<<"\t"<<input[j][k]<<"\t";
+      cout<<" "<<Form("%4.2e",input[j][k])<<" ";
+    }
+    if (j<input.size()-1){
+      cout<<"}\n";
+    }
+  }
+  cout<<"}}\n";
+}
+
+void displaySquareMatrix_h(vector<vector<Double_t>> input, Int_t size = -1){
+  if (size == -1){
+    size = input.size();
+  }
+  cout<<"{";
+  for (Int_t j = 0; j<size; j++){
+    cout<<"{";
+    for (Int_t k = 0; k<size; k++){
+      //cout<<"\t"<<input[j][k]<<"\t";
+      cout<<" "<<Form("%4.2e",input[j][k])<<" ";
+    }
+    if (j<input.size()-1){
+      cout<<"}\n";
+    }
+  }
+  cout<<"}}\n";
+}
+
+vector<vector<Double_t>> cofactor_h(vector<vector<Double_t>> input, vector<vector<Double_t>> temp, Int_t inRow, Int_t inColumn, Int_t size){
+  if (debug > 2) {
+    Printf("Input matrix to be cofactored:");
+    displaySquareMatrix_h(input,size);
+  }
   Int_t i = 0;
   Int_t j = 0;
 
-  for (Int_t row = 0; row<length; row++){
-    for (Int_t column = 0; column<length; column++){
+  for (Int_t row = 0; row<size; row++){
+    for (Int_t column = 0; column<size; column++){
       if (row!=inRow && column !=inColumn){
         temp[i][j++] = input[row][column];
-        if (j == length-1){
+        if (debug > 3) Printf("Cofactoring input[%d][%d] = %f ",row,column,input[row][column]);
+        if (j == size-1){
           j=0;
           i++;
         }
       }
     }
   }
+  if (debug > 3) {
+    Printf("Temp cofactored matrix:");
+    displaySquareMatrix_h(temp,size);
+  }
+  return temp;
 }
 
-Double_t determinant_h(vector<vector<Double_t>> input, Int_t subLength = 0){
+Double_t determinant_h(vector<vector<Double_t>> input, Int_t size){
+  if (debug>2) {
+    Printf("Matrix to have %dx%d determinant taken:",size,size);
+    displaySquareMatrix_h(input,size);
+  }
   Double_t determinant = 0.0;
-  if (input.size()==1){
+  if (size==1){
+    if (debug>2) Printf("Determinant = %f, size = %d",input[0][0], size);
     return input[0][0]; // Trivial scalar
   }
   vector<vector<Double_t>> temp; // For Cofactors
@@ -50,24 +98,29 @@ Double_t determinant_h(vector<vector<Double_t>> input, Int_t subLength = 0){
     temp2.clear();
   }
   Int_t sign = 1;                // For sign multiplier
-  for (Int_t i = 0 ; i < (input.size()-subLength) ; i++){
-    cofactor_h(input,temp,0,i,input.size()-subLength);
-    subLength++;
-    determinant+=sign*input[0][i]*determinant_h(temp,subLength);
+  for (Int_t i = 0 ; i < size ; i++){
+    temp=cofactor_h(input,temp,0,i,size);
+    determinant+=sign*input[0][i]*determinant_h(temp,size-1);
+    sign=-1*sign;
   }
+  if (debug>2) Printf("Determinant = %f, size = %d",determinant, size);
   return determinant;
 }
 
-void adjoint_h(vector<vector<Double_t>> input, vector<vector<Double_t>> adjoint){
+vector<vector<Double_t>> adjoint_h(vector<vector<Double_t>> input, vector<vector<Double_t>> adjoint){
+  if (debug > 2) {
+    Printf("Matrix to have its adjoint taken:");
+    displaySquareMatrix_h(input);
+  }
   if (input.size() == 1){
     adjoint[0][0] = input[0][0];
-    return;
+    return adjoint;
   }
   Int_t sign = 1;
   vector<vector<Double_t>> temp;
   vector<Double_t> temp2;
-  for (Int_t j = 0; j<input.size() ; j++){
-    for (Int_t k = 0; k<input.size() ; k++){
+  for (Int_t j = 0; j<input.size()-1 ; j++){
+    for (Int_t k = 0; k<input.size()-1 ; k++){
       temp2.push_back(0.0);
     }
     temp.push_back(temp2);
@@ -75,29 +128,43 @@ void adjoint_h(vector<vector<Double_t>> input, vector<vector<Double_t>> adjoint)
   }
   for (Int_t j = 0; j<input.size() ; j++){
     for (Int_t k = 0; k<input.size() ; k++){
-      cofactor_h(input,temp,j,k,input.size());
+      temp=cofactor_h(input,temp,j,k,input.size());
+      if (debug > 3) {
+        Printf("Temporary adjointing matrix:");
+        displaySquareMatrix_h(temp,temp.size());
+      }
       sign = ((j+k)%2==0)? 1: -1;
-      adjoint[k][j]=(sign)*(determinant_h(temp,1));
+      adjoint[k][j]=(sign)*(determinant_h(temp,input.size()-1));
     }
   }
+  return adjoint;
 }
 
-Bool_t inverse_h(vector<vector<Double_t>> input, vector<vector<Double_t>> output){
-  Double_t determinant = determinant_h(input,0);
+vector<vector<Double_t>> inverse_h(vector<vector<Double_t>> input, vector<vector<Double_t>> output){
+  if (debug > 2) {
+    Printf("Matrix to be inverted:");
+    displaySquareMatrix_h(input);
+  }
+  Double_t determinant = determinant_h(input,input.size());
+  if (debug > 2) {
+    Printf("Matrix determinant = %f",determinant);
+  }
+  vector<vector<Double_t>> errorOut;
+  errorOut.push_back({-999999.0});
   if (determinant == 0.0){
     Printf("Error: Singular curvature matrix, cannot find inverse");
-    return false;
+    return errorOut;
   }
   if (input.size()!=input[0].size()){
     Printf("Error: Non-square matrix, cannot find inverse");
-    return false;
+    return errorOut;
   }
-  adjoint_h(input, output);
+  output = adjoint_h(input, output);
   for (Int_t j = 0; j<input.size(); j++){
     for (Int_t k = 0; k<input[j].size(); k++){
       output[j][k] = output[j][k]/determinant;
     }
   }
-  return true;
+  return output;
 }
 #endif // __CAMMATRIX__
--- rootScripts/aggregator/regressor/camReg.hh
+++ rootScripts/aggregator/regressor/camReg.hh
@@ -12,6 +12,7 @@ void regress_h(TString tree = "mul", Int_t runNumber = 0, Int_t nRuns = -1, TStr
   nRuns     = getNruns_h(nRuns);
   vector<vector<string>> textFile = textFileParse_h(regInput,delim);
   TTree * oldTree = getTree_h(tree, runNumber, nRuns, "NULL");
+  TTreeReader oldTreeReader(oldTree);
   TTree * newTree = new TTree("reg"+tree,"Regressed "+tree+" tree");
   if (oldTree==0){
     Printf("Root file does not exist");
@@ -33,12 +34,12 @@ void regress_h(TString tree = "mul", Int_t runNumber = 0, Int_t nRuns = -1, TStr
   vector<TString> oldRespondingDataBranchList;
   vector<TString> oldRespondingErrorBranchList;
   TString errorBranchName  = "ErrorFlag";
-  TString oldTreeErrorFlag = "0";
+  Double_t oldTreeErrorFlag = 0.0;
   Bool_t resp  = false;
   Int_t nresp  = 0;
   Bool_t manip = false;
   Int_t nmanip = 0;
-  if (debug > 2) Printf("Data structures initialized");
+  if (debug > -1) Printf("Data structures initialized");
   for (UInt_t listEntryN = 0; listEntryN < textFile.size(); listEntryN++){
     if (textFile[listEntryN][0] == "Global"){
       if (debug > 2) Printf("Branch %s",textFile[listEntryN][1].c_str());
@@ -84,30 +85,47 @@ void regress_h(TString tree = "mul", Int_t runNumber = 0, Int_t nRuns = -1, TStr
   parameters.push_back(0.0); // Push back the physics asymmetry value placeholder at nmanip+1 position... assume it is trivially 0 for first pass
   weighting.push_back(1.0); // Push back the physics asymmetry relative weighting factor for uncertainty calculations
   nmanip++; // The physics asymmetry value adds ++ to the parameters of the fit
+  Int_t nmanipInputs = nmanip-1;
+
+  vector<TTreeReaderValue<Double_t>> oldRespondingValuesReader;
+  vector<TTreeReaderValue<Double_t>> oldRespondingErrorsReader;
+  vector<TTreeReaderValue<Double_t>> oldManipulatedValuesReader;
+  vector<TTreeReaderValue<Double_t>> oldManipulatedErrorsReader;
+
   for(Int_t iBranch = 0; iBranch < oldManipulatedDataBranchList.size(); iBranch++) {
-    oldTree->SetBranchAddress(oldManipulatedDataBranchList[iBranch],&oldManipulatedValues[iBranch]);
-    oldTree->SetBranchAddress(oldManipulatedErrorBranchList[iBranch],&oldManipulatedErrors[iBranch]);
+    TTreeReaderValue<Double_t> temp1(oldTreeReader,oldManipulatedDataBranchList[iBranch]);
+    TTreeReaderValue<Double_t> temp2(oldTreeReader,oldManipulatedErrorBranchList[iBranch]);
+    oldManipulatedValuesReader.push_back(temp1);
+    oldManipulatedErrorsReader.push_back(temp2);
+    //oldTree->SetBranchAddress(oldManipulatedDataBranchList[iBranch],&oldManipulatedValues[iBranch]);
+    //oldTree->SetBranchAddress(oldManipulatedErrorBranchList[iBranch],&oldManipulatedErrors[iBranch]);
   }
   for(Int_t iBranch = 0; iBranch < oldRespondingDataBranchList.size(); iBranch++) {
-    oldTree->SetBranchAddress(oldRespondingDataBranchList[iBranch],&oldRespondingValues[iBranch]);
-    oldTree->SetBranchAddress(oldRespondingErrorBranchList[iBranch],&oldRespondingErrors[iBranch]);
+    TTreeReaderValue<Double_t> temp1(oldTreeReader,oldRespondingDataBranchList[iBranch]);
+    TTreeReaderValue<Double_t> temp2(oldTreeReader,oldRespondingErrorBranchList[iBranch]);
+    oldRespondingValuesReader.push_back(temp1);
+    oldRespondingErrorsReader.push_back(temp2);
+    //oldTree->SetBranchAddress(oldRespondingDataBranchList[iBranch],&oldRespondingValues[iBranch]);
+    //oldTree->SetBranchAddress(oldRespondingErrorBranchList[iBranch],&oldRespondingErrors[iBranch]);
     newTree->Branch(          newRegressedBranchList[iBranch],&newRegressedValues[iBranch]);
   }
   oldTree->SetBranchAddress(errorBranchName,&oldTreeErrorFlag);
   newTree->SetBranchStatus("*",1);
   oldTree->SetBranchStatus("*",1);
+  if (debug > -1) Printf("Branches initialized");
 
   Double_t rate = 1.0e9;
   Double_t integrationFreq = 240.0;
   Double_t uncertainty = 1.0/sqrt(rate/integrationFreq);
 
+  //FIXME make these into branches that get stored in output tree instead of large vectors here
   vector<Double_t> f; // The functional value integrated over all data entries
   vector<Double_t> s; // The sigma value integrated over all data entries
-  vector<Double_t> chi2i;
+  vector<Double_t> chi2;
   Double_t fi = 0.0; // The functional value per data entry
   Double_t si2 = uncertainty*uncertainty;// The total sigma value per data entry
   Double_t si = 0.0; // The total sigma value per data entry
-  Double_t chi2 = 0.0;
+  Double_t chi2i = 0.0;
   vector<Double_t> dfi;   // The first derivative of the functional value per data entry
   vector<Double_t> dsi;   // The first derivative of the sigma value per data entry
   vector<Double_t> dsi2;  // The first derivative of the sigma value per data entry
@@ -117,40 +135,57 @@ void regress_h(TString tree = "mul", Int_t runNumber = 0, Int_t nRuns = -1, TStr
   vector<vector<vector<Double_t>>> dddfi; // The third derivative of the functional value per data entry
 
   vector<Double_t> beta; // The x^2 slope vector
+  vector<Double_t> betai; // The x^2 slope vector
   vector<vector<Double_t>> alpha; // The x^2 curvature matrix
+  vector<vector<Double_t>> alphai; // The x^2 curvature matrix
   vector<vector<Double_t>> covariance; // The Covariance matrix = alpha^-1
+  vector<vector<Double_t>> covariancei; // The Covariance matrix = alpha^-1
   vector<Double_t> delta_parameters; // The change in parameter vector = alpha^-1*beta
   vector<Double_t> placeholder;
   vector<vector<Double_t>> placeholder2;
-  for (Int_t j = 0; j<nmanip; j++){
-    for (Int_t k = 0; j<nmanip; k++){
-      for (Int_t l = 0; j<nmanip; l++){
-        placeholder.push_back(0.0); // Make a vector of 0s
-      }
-      placeholder2.push_back(placeholder); // Make a matrix of vectors of 0s
-      placeholder.clear();
-    }
+  vector<vector<vector<Double_t>>> placeholder3;
+  if (debug > -1) Printf("Empty matrices");
+  for (Int_t l = 0; l<nmanip; l++){
+    placeholder.push_back(0.0); // Make a vector of 0s
     dfi.push_back(0.0);
     dsi.push_back(0.0);
     dsi2.push_back(0.0);
     beta.push_back(0.0);
+    betai.push_back(0.0);
     delta_parameters.push_back(0.0);
+  }
+  for (Int_t k = 0; k<nmanip; k++){
+    placeholder2.push_back(placeholder); // Make a matrix of vectors of 0s
     ddfi.push_back(placeholder);
     ddsi.push_back(placeholder);
     ddsi2.push_back(placeholder);
     alpha.push_back(placeholder);
+    alphai.push_back(placeholder);
     covariance.push_back(placeholder);
+  }
+  for (Int_t j = 0; j<nmanip; j++){
     dddfi.push_back(placeholder2); // Make a 3-tensor of matrices of vectors of 0s
-    placeholder2.clear();
+    placeholder3.push_back(placeholder2);
   }
 
   Int_t fitN = 0;
   Int_t errorFlag = 0;
   Int_t numEntries = oldTree->GetEntries();
-  for (int i = 0; i < numEntries; i++) { // Loop over the input file's entries
-    oldTree->GetEntry(i);
+  if (debug > -1) Printf("Looping over %d entries",numEntries);
+  Int_t i = 0;
+  while(oldTreeReader.Next()){
+  //for (int i = 0; i < numEntries; i++)  // Loop over the input file's entries
+    //oldTree->GetEntry(i);
     if (oldTreeErrorFlag==0){
       for (Int_t j = 0 ; j<nmanip ; j++){ // Loop over fit parameters j
+        if (debug > 3) Printf("Looping, j = %d",j);
+        if (j<nmanipInputs){
+          oldManipulatedValues[j]=*oldManipulatedValuesReader[j];
+          oldManipulatedErrors[j]=*oldManipulatedErrorsReader[j];
+        }
+        else {
+          oldManipulatedValues[j]=1.0; // User input values here;
+        }
         if(oldManipulatedErrors[j]==0){
           fi += parameters[j]*oldManipulatedValues[j]; // Functional form of f
           dfi[j] = 1*oldManipulatedValues[j]; // Functional form of first derivative of f
@@ -162,77 +197,108 @@ void regress_h(TString tree = "mul", Int_t runNumber = 0, Int_t nRuns = -1, TStr
           } // end l
         } // end k
         else {
+          if (debug > 2) Printf("Entry %d has an error in input %s",i,(const char*)oldManipulatedDataBranchList[j]);
           errorFlag = 1;
           continue; // Abort this entry
         }
       } // end j (f)
+      if((*oldRespondingErrorsReader[fitN])!=0){ 
+        if (debug > 2) Printf("Entry %d has an error in input %s",i,(const char*)oldRespondingDataBranchList[fitN]);
+        errorFlag = 1; 
+      }
+      if (errorFlag==1){ 
+        errorFlag = 0;
+        continue; 
+      }
       for (Int_t j = 0 ; j<nmanip ; j++){ // Loop over fit parameters j
-        if(oldManipulatedErrors[j]==0){
-          si2 += covariance[j][j]*dfi[j]*dfi[j];
-          for (Int_t k = j+1; k<nmanip ; k++){ // Loop over fit parameters k
-            si2  += 2*dfi[j]*dfi[k]*covariance[j][k];
+        si2 += covariance[j][j]*dfi[j]*dfi[j];
+        for (Int_t k = j+1; k<nmanip ; k++){ // Loop over fit parameters k
+          si2  += 2*dfi[j]*dfi[k]*covariance[j][k];
+        }
+        si = sqrt(si2);
+        //chi2i += (oldRespondingValues[fitN]-fi)*(oldRespondingValues[fitN]-fi)/(si2);
+        if (si2 == 0){
+          chi2i += (*oldRespondingValuesReader[fitN]-fi)*(*oldRespondingValuesReader[fitN]-fi);
+        }
+        else if (si2 > 0.0){
+          chi2i += (*oldRespondingValuesReader[fitN]-fi)*(*oldRespondingValuesReader[fitN]-fi)/(si2);
+        }
+        else {
+          continue;
+        }
+      }
+      f.push_back(fi);
+      s.push_back(si);
+      chi2.push_back(chi2i);
+      for (Int_t j = 0 ; j<nmanip ; j++){ // Loop over fit parameters j
+        for (Int_t k = 0; k<nmanip ; k++){ // Loop over fit parameters k
+          dsi2[j] += 2*covariance[k][k]*dfi[k]*ddfi[k][j];
+          for (Int_t l = k+1; l<nmanip ; l++){ // Loop over fit parameters l
+            dsi2[j] += 2*covariance[k][l]*((ddfi[j][k]*dfi[l])+(ddfi[j][l]*dfi[k]));
           }
-          si = sqrt(si2);
-          chi2 += (oldRespondingValues[fitN]-fi)*(oldRespondingValues[fitN]-fi)/(si2);
+          dsi[j] = dsi2[j]/(2*si);
         }
       }
       for (Int_t j = 0 ; j<nmanip ; j++){ // Loop over fit parameters j
-        if(oldManipulatedErrors[j]==0){
-          for (Int_t k = 0; k<nmanip ; k++){ // Loop over fit parameters k
-            dsi2[j] += 2*covariance[k][k]*dfi[k]*ddfi[k][j];
-            for (Int_t l = k+1; l<nmanip ; l++){ // Loop over fit parameters l
-              dsi2[j] += 2*covariance[k][l]*((ddfi[j][k]*dfi[l])+(ddfi[j][l]*dfi[k]));
+        for (Int_t k = 0; k<nmanip ; k++){ // Loop over fit parameters k
+          for (Int_t l = 0; l<nmanip ; l++){ // Loop over fit parameters l
+            ddsi2[j][k] += 2*covariance[l][l]*((ddfi[l][k]*dfi[j])+(ddfi[j][k]*dfi[l]));
+            for (Int_t m = l+1; m<nmanip ; m++){
+              ddsi2[j][k] += 2*covariance[l][m]*((dddfi[j][k][l]*dfi[m])+(ddfi[l][j]*ddfi[m][k])+(dddfi[j][m][k]*dfi[l])+(ddfi[j][m]*ddfi[l][k]));
             }
-            dsi[j] = dsi2[j]/(2*si);
+            ddsi[j][k] = ((ddsi2[j][k]/(2*si))-(dsi2[j]*dsi2[k]/(4*si*si*si)));
           }
         }
       }
       for (Int_t j = 0 ; j<nmanip ; j++){ // Loop over fit parameters j
-        if(oldManipulatedErrors[j]==0){
-          for (Int_t k = 0; k<nmanip ; k++){ // Loop over fit parameters k
-            for (Int_t l = 0; l<nmanip ; l++){ // Loop over fit parameters l
-              ddsi2[j][k] += 2*covariance[l][l]*((ddfi[l][k]*dfi[j])+(ddfi[j][k]*dfi[l]));
-              for (Int_t m = l+1; m<nmanip ; m++){
-                ddsi2[j][k] += 2*covariance[l][m]*((dddfi[j][k][l]*dfi[m])+(ddfi[l][j]*ddfi[m][k])+(dddfi[j][m][k]*dfi[l])+(ddfi[j][m]*ddfi[l][k]));
-              }
-              ddsi[j][k] = ((ddsi2[j][k]/(2*si))-(dsi2[j]*dsi2[k]/(4*si*si*si)));
-            }
-          }
+        betai[j] += (2*((*oldRespondingValuesReader[fitN]-fi)*(*oldRespondingValuesReader[fitN]-fi)/(si*si*si))*dsi[j])+(2*(*oldRespondingValuesReader[fitN]-fi)*dfi[j]/(si*si));
+        beta[j] += 1e-6*betai[j];
+        for (Int_t k = 0; k<nmanip ; k++){ // Loop over fit parameters k
+          alphai[j][k] += 0.5*(4*(dsi[j]*(*oldRespondingValuesReader[fitN]-fi)*dfi[k]/(si*si*si))+(6*(*oldRespondingValuesReader[fitN]-fi)*(*oldRespondingValuesReader[fitN]-fi)*dsi[j]*dsi[k]/(si*si*si*si))+(-2*(*oldRespondingValuesReader[fitN]-fi)*(*oldRespondingValuesReader[fitN]-fi)*ddsi[j][k]/(si*si*si))+(4*(*oldRespondingValuesReader[fitN]-fi)*dsi[k]*dfi[j]/(si*si*si))+(2*(dfi[j]*dfi[k]/(si*si)))+(-2*(*oldRespondingValuesReader[fitN]-fi)*ddfi[j][k]/(si*si)));
+          alpha[j][k] += 1e-6*alphai[j][k];
+          if (debug > 4) Printf("Beta = %f, Alpha = %f",betai[j],alpha[j][k]);
         }
       }
-      for (Int_t j = 0 ; j<nmanip ; j++){ // Loop over fit parameters j
-        if(oldManipulatedErrors[j]==0){
-          beta[j] += (2*((oldRespondingValues[fitN]-fi)*(oldRespondingValues[fitN]-fi)/(si*si*si))*dsi[j])-(2*(oldRespondingValues[fitN]-fi)*dfi[j]/(si*si));
-          for (Int_t k = 0; k<nmanip ; k++){ // Loop over fit parameters k
-            alpha[j][k] += 0.5*(-4*(dsi[j]*(oldRespondingValues[fitN]-fi)*dfi[k]/(si*si*si))+(6*(oldRespondingValues[fitN]-fi)*(oldRespondingValues[fitN]-fi)*dsi[j]*dsi[k]/(si*si*si*si))+(-2*(oldRespondingValues[fitN]-fi)*(oldRespondingValues[fitN]-fi)*ddsi[j][k]/(si*si*si))+(4*(oldRespondingValues[fitN]-fi)*dsi[k]*dfi[j]/(si*si*si))+(2*(dfi[j]*dfi[k]/(si*si)))+(-2*(oldRespondingValues[fitN]-fi)*ddfi[j][k]/(si*si)));
-          }
-        }
-      }
-      /*
-      // invert alpha
-      displayMatrix_h(alpha);
-      if (!inverse_h(alpha,covariance)){
-        Printf("Error, uninvertable matrix");
-        return;
-      }
-      displayMatrix_h(covariance);
-      // multiply epsilon*beta = a vector
-      for (Int_t j = 0 ; j<beta.size(); j++){
-        for (Int_t k = 0 ; k<beta.size(); k++){
-          delta_parameters[j]+=covariance[j][k]*beta[k];
-        }
-      }
+      if (debug > -1) Printf("Functional value at event %d = %f, sigma = %f, chi2 = %f",i,fi,si,chi2i);
+
       // End storing values
-      if (errorFlag==0){
-        f.push_back(fi);
-      }
+      dsi.swap(placeholder);
+      dsi2.swap(placeholder);
+      dfi.swap(placeholder);
+      betai.swap(placeholder);
+      alphai.swap(placeholder2);
+      ddsi.swap(placeholder2);
+      ddsi2.swap(placeholder2);
+      ddfi.swap(placeholder2);
+      dddfi.swap(placeholder3);
       errorFlag = 0;
       fi=0.0;
-      si2=0.0;
-      */
+      chi2i=0.0;
+      si=0.0;
+      si2=uncertainty*uncertainty;
+      
+      if (debug > 4) Printf("Done entry %d",i);
+      i++;
+    }
+  }
+  // invert alpha
+  Printf("Alpha matrix: ");
+  displayMatrix_h(alpha);
+  Printf("Beta vector: ");
+  displayVector_h(beta);
+  covariance = inverse_h(alpha,covariance);
+  if (covariance[0][0]==-999999.0){
+    Printf("Error, uninvertable matrix");
+    return;
+  }
+  Printf("Covariance matrix: ");
+  displayMatrix_h(covariance);
+  // multiply epsilon*beta = a vector
+  for (Int_t j = 0 ; j<beta.size(); j++){
+    for (Int_t k = 0 ; k<beta.size(); k++){
+      delta_parameters[j]+=covariance[j][k]*beta[k];
     }
   }
   //writeFile_h("test_n_data",n_data,runNumber,nRuns);
-  chi2=0.0;
 }
 #endif // __CAMREG__
