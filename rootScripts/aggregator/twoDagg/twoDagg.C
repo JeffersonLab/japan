28babc8a1debff93566c2046280e8031f16e8101
rahmans1 <rahmans@myumanitoba.ca>
2019-05-22 02:29:14 -0400

    wrapper and script for creating a pdf file comprising of variables vs runnumber plots. ./twoDwrapper.sh -f twoDinput.txt -a location/run_aggregator.root will create a pdf file in location/

---
+++ rootScripts/aggregator/twoDagg/twoDagg.C
@@ -0,0 +1,57 @@
+#include "TString.h"
+#include "TTree.h"
+#include "TChain.h"
+
+
+TGraphErrors gDraw(TTree *T, TString var, TString plot, Bool_t  good)
+{
+TString ok="";
+Int_t nEntries=0;
+if (good){
+  ok += Form("%s_%s>-1e99 && %s_%s<1e99",plot.Data(), var.Data(),plot.Data(), var.Data());
+} else {
+  ok += Form("%s_%s==-1e99 || %s_%s==1e99", plot.Data(), var.Data(), plot.Data(), var.Data());
+}
+
+nEntries=T->Draw(Form("run_number:%s_%s:%s_%s_error", plot.Data(),var.Data(),plot.Data(),var.Data()),ok.Data(), "goff");
+TGraphErrors g(nEntries, T->GetV1(), T->GetV2(), 0, T->GetV3());
+g.SetTitle(Form("%s_%s vs run", plot.Data(), var.Data()));
+
+return g;
+} 
+
+
+void twoDagg(TString var, TString agg){
+
+
+TChain *T=new TChain("agg");
+T->Add(agg.Data());
+TCanvas *c=new TCanvas("c", "c", 1200, 900);
+c->Divide(2,2);
+
+c->cd(1);
+gPad->SetGrid();
+TGraphErrors gmean= gDraw(T,var, "mean", true);
+gmean.Draw("ap");
+c->cd(2);
+gPad->SetGrid();
+TGraphErrors gmeanEC= gDraw(T,var, "mean", false);
+gmeanEC.Draw("ap");
+gmeanEC.SetMarkerColor(kRed);
+gmeanEC.SetLineColor(kRed);
+c->cd(3);
+gPad->SetGrid();
+TGraphErrors grms=gDraw(T,var, "rms", true);
+grms.Draw("ap");
+c->cd(4);
+gPad->SetGrid();
+TGraphErrors grmsEC=gDraw(T,var,"rms", false);
+grmsEC.Draw("ap");
+grmsEC.SetMarkerColor(kRed);
+grmsEC.SetLineColor(kRed);
+
+c->Print(Form("%s.png",var.Data()));
+return 0;
+}
+
+
