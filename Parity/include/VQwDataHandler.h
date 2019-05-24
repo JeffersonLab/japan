f7f1cad1dd6ff7759b57b59d5bc7858392907e1f
Wouter Deconinck <wdconinc@gmail.com>
2019-05-24 13:26:47 -0400

    Allow for prefix = set1_ flags in data handlers
    This will prepend to the name of leafs in the tree, and should
    allow for having multiple correlator sets run simultaneously.
    
    E.g. prefix = kitchensink_ could be everything, while empty
    prefix could be the default regression set.
    
    Also works for other data handlers e.g. correctors.

--- Parity/include/VQwDataHandler.h
+++ Parity/include/VQwDataHandler.h
@@ -37,7 +37,7 @@ class VQwDataHandler:  virtual public VQwDataHandlerCloneable {
     typedef std::vector< VQwHardwareChannel* >::iterator Iterator_HdwChan;
     typedef std::vector< VQwHardwareChannel* >::const_iterator ConstIterator_HdwChan;
 
-    VQwDataHandler(const TString& name):fName(name),fKeepRunningSum(kFALSE){}
+    VQwDataHandler(const TString& name):fName(name),fPrefix(""),fKeepRunningSum(kFALSE) {}
     VQwDataHandler(const VQwDataHandler &source);
 
     virtual void ParseConfigFile(QwParameterFile& file);
@@ -109,6 +109,8 @@ class VQwDataHandler:  virtual public VQwDataHandlerCloneable {
    std::string fMapFile;
    std::string fTreeName;
    std::string fTreeComment;
+
+   std::string fPrefix;
 
    TString run_label;
 
