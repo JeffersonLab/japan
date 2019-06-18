5bfad53e03d35e755c8268aecc89961723a260ef
rahmans1 <rahmans@myumanitoba.ca>
2019-06-18 15:00:47 -0400

    initial framework for python aggregator

--- rootScripts/aggregator/regout/miniagg.C
+++ rootScripts/aggregator/regout/miniagg.C
@@ -5,7 +5,6 @@
 void displayColumns(ROOT::RDataFrame d){
 
 auto colNames=d.GetColumnNames();
-std::cout<< "hello";
 for (auto &&colName:colNames){
      std::cout<<colName<<std::endl;
 }
@@ -18,31 +17,40 @@ Int_t miniagg(TString runlist, TString varlist, TString output){
 TString filename1="/adaqfs/home/apar/pking/example_correlator_outputblueR1296new.slope.root";
 TString filename2="/chafs2/work1/apar/japanOutput/prexALL_2332.000.root";
 TString varname="";
-Int_t nRuns=20;
+
 
 ROOT::RDataFrame d=ROOT::RDataFrame("mul",filename2.Data());
 
-
+//displayColumns(d);
 std::vector<TString> name;
 name.push_back("asym_sam1");
 name.push_back("asym_sam2");
 
-std::vector<Float_t> means;
+std::vector<Float_t> mean;
+std::vector<Float_t> err_mean;
+std::vector<Float_t> rms;
+std::vector<Float_t> err_rms;
+
+
 
 for (auto it=name.begin();it!=name.end();it++){
-Float_t sum=0;
-Int_t n=0;
-d.Foreach([&sum, &n](double b){++n; sum+=b; }, {Form("%s.hw_sum",(*it).Data())});
-means.push_back(sum/n);
+auto h=d.Histo1D(Form("%s.hw_sum",(*it).Data());
+mean.push_back(h->GetMean());
+err_mean.push_back(h->GetMeanError());
+rms.push_back(h->GetRMS());
+err_rms.push_back(h->GetRMSError());
+h->SetDirectory(0);
 }
 
 
 ROOT::RDataFrame summary(1);
-auto newsum=summary.Define(name[0].Data(), [&means]() {return means[0];});
-  
-newsum=newsum.Define(name[1].Data(), [&means]() {return means[1];});
+auto newsum=summary.Define("h1", [&means]() {return means[0];});
+auto newsum1=newsum.Define("h1_t", [&means]() {return means[1];});
+auto newsum2=newsum1.Define("h2", [&means]() {return means[2];});
+auto newsum3=newsum2.Define("h2_t", [&means]() {return means[3];});
 
-newsum.Snapshot("tree", "tree.root");
+
+newsum3.Snapshot("tree", "tree.root");
 
 return 0;
 }
