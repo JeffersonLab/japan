5e4501823c2fe49b656fd913d34c4f406380f488
rahmans1 <rahmans@myumanitoba.ca>
2019-06-20 01:59:52 -0400

    created frame work to output aggregator result to a pdf

--- rootScripts/aggregator/bmodagg/bmodagg.py
+++ rootScripts/aggregator/bmodagg/bmodagg.py
@@ -1,8 +1,10 @@
 import sys
 import os
 import ROOT as R
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from array import array
 
 class bmoddata:
   #parses Tao's csv file output and initializes a bmod structure storing the information
@@ -50,7 +52,25 @@ for f in range(0,filecount):
 
   for l in range(0,varcount):
     df_varcut=df[df.Variable==varlist[l]]
-   
+    plotobj.updatelist(pobj[l],df_varcut['RunID'].tolist(), df_varcut['CycleID'].tolist(), df_varcut['coeff'].tolist(),df_varcut['error'].tolist())
+
+output=R.TFile("bmod_agg_summary.root","recreate")
+
+for l in range(0,varcount):
+    n=len(pobj[l].run)
+    c=R.TCanvas(pobj[l].name, pobj[l].name, 800,600)
+    graph=R.TGraphErrors(n,array('f',pobj[l].run), array('f',pobj[l].coeff), array('f',[0]*n), array('f',pobj[l].error)  )
+    graph.SetTitle(pobj[l].name+" vs run/cycle")
+    graph.Draw("AP")
+    c.Print("fig/"+pobj[l].name+".png")
+    #graph.Write(pobj[l].name)
+
+    
+output.Close()
+
+
+
+
 
   
 
