5d96e0ab37c0e12255afd9f5b38e83f1fcd2c645
Wouter Deconinck <wdconinc@gmail.com>
2019-07-09 15:27:42 -0400

    LinRegBevPeb operator+= for accumulation of LRB data
    This will be required for accumulator functionality for bursts in the
    correlator data handler.

--- Parity/include/LinReg_Bevington_Pebay.h
+++ Parity/include/LinReg_Bevington_Pebay.h
@@ -86,6 +86,49 @@ class LinRegBevPeb {
 
   double  getUsedEve(){ return fGoodEventNumber;}
 
+  // Addition-assignment
+  LinRegBevPeb& operator+=(const LinRegBevPeb& rhs)
+  {
+    // If X = A + B, then
+    //   Cov[X] = Cov[A] + Cov[B] + (E[x_A] - E[x_B]) * (E[y_A] - E[y_B]) * n_A * n_B / n_X
+    // Ref: E. Schubert, M. Gertz (9 July 2018).
+    // "Numerically stable parallel computation of (co-)variance".
+    // SSDBM '18 Proceedings of the 30th International Conference
+    // on Scientific and Statistical Database Management.
+    // https://doi.org/10.1145/3221269.3223036
+
+    // Deviations from mean
+    TVectorD delta_y(mMY - rhs.mMY);
+    TVectorD delta_p(mMP - rhs.mMP);
+
+    // Update covariances
+    Double_t alpha = fGoodEventNumber * rhs.fGoodEventNumber
+                  / (fGoodEventNumber + rhs.fGoodEventNumber);
+    mVYY += rhs.mVYY;
+    mVYY.Rank1Update(delta_y, delta_y, alpha);
+    mVPY += rhs.mVPY;
+    mVPY.Rank1Update(delta_p, delta_y, alpha);
+    mVPP += rhs.mVPP;
+    mVPP.Rank1Update(delta_p, delta_p, alpha);
+
+    // Update means
+    Double_t beta = rhs.fGoodEventNumber / (fGoodEventNumber + rhs.fGoodEventNumber);
+    mMY += delta_y * beta;
+    mMP += delta_p * beta;
+
+    fGoodEventNumber += rhs.fGoodEventNumber;
+
+    return *this;
+  }
+
+  // Addition using addition-assignment
+  // friends defined inside class body are inline and are hidden from non-ADL lookup
+  friend LinRegBevPeb operator+(LinRegBevPeb lhs,  // passing lhs by value helps optimize chained a+b+c
+                   const LinRegBevPeb& rhs) // otherwise, both parameters may be const references
+  {
+    lhs += rhs; // reuse compound assignment
+    return lhs; // return the result by value (uses move constructor)
+  }
 
   /// \brief Output stream operator
   friend std::ostream& operator<< (std::ostream& stream, const LinRegBevPeb& h);
