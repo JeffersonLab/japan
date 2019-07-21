bf03c3e12ac723e32dca0c30e76f9c7e7632f1a9
Cameron Clarke <cameronc137@gmail.com>
2019-07-21 15:01:26 -0400

    Updating camHist.hh with a regular old T->Draw method... why this wasn't earlier I have no idea...  --- relies on TCut being TString and being from getCuts_h() (be careful)

--- rootScripts/aggregator/camHist.hh
+++ rootScripts/aggregator/camHist.hh
@@ -229,6 +229,62 @@ void writeInt_leafHist_h(TString tree = "mul", TString branch = "asym_vqwk_04_0c
   }
 }
 
+
+// NEW FIXME
+
+
+void writeMeanRms_h(TString tree = "mul", TString draw = "asym_vqwk_04_0ch0", TString cut = "defaultCut", Int_t overWriteCut = 0, Int_t runNumber = 0, Int_t minirunNumber = -2, Int_t splitNumber = -1, Int_t nRuns = -1){
+  runNumber = getRunNumber_h(runNumber);
+  splitNumber = getSplitNumber_h(splitNumber);
+  minirunNumber = getMinirunNumber_h(minirunNumber);
+  nRuns     = getNruns_h(nRuns);
+  TString mean = "NULL";
+  TString mean_error  = "NULL";
+  TString rms = "NULL";
+  TString rms_error = "NULL";
+  Double_t data_mean = 0;
+  Double_t data_mean_error = 0;
+  Double_t data_rms = 0;
+  Double_t data_rms_error = 0;
+  TChain * Tree = getTree_h(tree, runNumber, minirunNumber, splitNumber, nRuns, filenamebase);
+  TString cut = getCuts_h(cut, overWriteCut, "NULL"); // FIXME in postPan era, branchToCheck in cuts method is not even used, but when it is, it will need to be parsed correctly - probably just ignore this from now on and make Device_Error_Code something done explicitly (since so many variables in Japan actually aren't devices)
+  Tree->Draw(draw,cut,overWriteCut,mode,runNumber,minirunNumber,splitNumber,nRuns);
+  TH1 * hMeanRms = (TH1*)gROOT->FindObject("htemp");
+  if (hMeanRms==0)
+  {
+    Printf("Error, writeMeanRms_leafHist_h: Histogram failed");
+  }
+  else {
+    data_mean = hMeanRms->GetMean(1);
+    data_mean_error = hMeanRms->GetMeanError(1);
+    data_rms = hMeanRms->GetRMS(1);
+    data_rms_error = hMeanRms->GetRMSError(1);
+  }
+
+  if (debug>1) Printf("Run %d mean %s: %f+-%f",runNumber,(const char*)mean,data_mean,data_mean_error);
+  if (debug>1) Printf("Run %d rms %s: %f+-%f",runNumber,(const char*)rms,data_rms,data_rms_error);
+  if (aggregatorStatus){
+    writeFile_h(mean,data_mean,runNumber,splitNumber,nRuns);
+    writeFile_h(mean_error,data_mean_error,runNumber,splitNumber,nRuns);
+    writeFile_h(rms,data_rms,runNumber,splitNumber,nRuns);
+    writeFile_h(rms_error,data_rms_error,runNumber,splitNumber,nRuns);
+  }
+  if (alarmStatus){
+    // Then the alarm handler wants to receive the output in stdout
+    Printf("%s=%f",(const char*)mean,data_mean);
+    Printf("%s=%f",(const char*)mean_error,data_mean_error);
+    Printf("%s=%f",(const char*)rms,data_rms);
+    Printf("%s=%f",(const char*)rms_error,data_rms_error);
+  }
+}
+
+
+
+
+// END NEW FIXME
+
+
+
 void writeMeanRms_leafHist_h(TString tree = "mul", TString branch = "asym_vqwk_04_0ch0", TString leaf = "hw_sum", TString cut = "defaultCut", Int_t overWriteCut = 0, TString mode = "defaultHist", Int_t runNumber = 0, Int_t minirunNumber = -2, Int_t splitNumber = -1, Int_t nRuns = -1){
   runNumber = getRunNumber_h(runNumber);
   splitNumber = getSplitNumber_h(splitNumber);
