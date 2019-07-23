ee69d87d06082ee68844c211d289d4ed8c7b90e1
rahmans1 <rahmans@myumanitoba.ca>
2019-07-23 14:21:15 -0400

    Updating "mean" and "rms" name stripping to work properly

--- rootScripts/aggregator/twoDagg/twoDagg.C
+++ rootScripts/aggregator/twoDagg/twoDagg.C
@@ -9,12 +9,18 @@ TGraphErrors gDraw(TTree *T, TString var, TString plot, Bool_t  good)
 TString ok="";
 Int_t nEntries=0;
 if (good){
-  ok += Form("%s_%s!=0 || %s_%s_error!=0",plot.Data(), var.Data(),plot.Data(), var.Data());
+  ok += Form("%s_%s!=0 || %s_%s_error!=0", var.Data(), plot.Data(), var.Data(), plot.Data());
 } else {
-  ok += Form("%s_%s==0 && %s_%s_error==0", plot.Data(), var.Data(), plot.Data(), var.Data());
+  ok += Form("%s_%s==0 && %s_%s_error==0", var.Data(), plot.Data(), var.Data(), plot.Data());
 }
 
-nEntries=T->Draw(Form("run_number:%s_%s:%s_%s_error", plot.Data(),var.Data(),plot.Data(),var.Data()),ok.Data(), "goff");
+TString unit="1";
+if (var.Contains("asym")){
+	if (plot=="mean") unit="ppb";
+        if (plot=="rms") unit="ppm";
+}
+
+nEntries=T->Draw(Form("run_number:%s_%s/%s:%s_%s_error/%s", var.Data(), plot.Data(), unit.Data(), var.Data(),plot.Data(), unit.Data()),ok.Data(), "goff");
 
 Double_t run[nEntries];
 for(int i=0; i<nEntries;i++){
@@ -33,7 +39,7 @@ for(int i=0; i<nEntries;i++){
 }
 Double_t par;
 g.Fit("pol0");
-g.SetTitle(Form("%s_%s vs run", plot.Data(), var.Data()));
+g.SetTitle(Form("%s_%s/%s vs run", var.Data(), plot.Data(), unit.Data()));
 g.SetMarkerStyle(20);
 //TF1 *fit=g.GetFunction("pol0");
 //printf("%3.1f",fit->GetParameter(0));
