ed146fd297bb2a69e079a475076fecf493ab1068
Cameron Clarke <cameronc137@gmail.com>
2019-07-27 15:53:52 -0400

    Updating loop to do all miniruns at the same time I used an & which could very well cause problems, but it should work for now

--- rootScripts/aggregator/wrapper/loop-oneRun.py
+++ rootScripts/aggregator/wrapper/loop-oneRun.py
@@ -41,7 +41,7 @@ if int(fullruns) == 0:
   start = 0
   for mini in range(start,nMiniRuns):
     print("Looking at mini run # = "+str(mini))
-    os.system("./wrapper.sh -f "+str(devicelist)+" -r "+str(run)+" -m "+str(mini)+" -s 000 -n "+str(slug)) # FIXME This -n 1 should be the Slug Number
+    os.system("./wrapper.sh -f "+str(devicelist)+" -r "+str(run)+" -m "+str(mini)+" -s 000 -n "+str(slug)+"&") # FIXME backgrounding here so that it will do all miniruns at once
 else: # Do full run only (obviously this can be editted to do both in one go... but people want them separate - once we get the agg-rootfile names done correctly we can handle this internally to camguin)
     print("Looking at Full run")
     os.system("./wrapper.sh -f "+str(devicelist)+" -r "+str(run)+" -m "+str(-1)+" -s 000 -n "+str(slug))
--- rootScripts/aggregator/wrapper/loop.py
+++ rootScripts/aggregator/wrapper/loop.py
@@ -58,7 +58,7 @@ for line in lines:
     start = 0
     for mini in range(start,nMiniRuns):
       print("Looking at mini run # = "+str(mini))
-      os.system("./wrapper.sh -f "+str(devicelist)+" -r "+str(line)+" -m "+str(mini)+" -s 000 -n "+str(slug)) # FIXME This -n 1 should be the Slug Number
+      os.system("./wrapper.sh -f "+str(devicelist)+" -r "+str(line)+" -m "+str(mini)+" -s 000 -n "+str(slug)+"&") # FIXME backgrounding here so that it will do all miniruns at once
   else: # Do full run only (obviously this can be editted to do both in one go... but people want them separate - once we get the agg-rootfile names done correctly we can handle this internally to camguin)
       print("Looking at Full run")
       os.system("./wrapper.sh -f "+str(devicelist)+" -r "+str(line)+" -m "+str(-1)+" -s 000 -n "+str(slug))
